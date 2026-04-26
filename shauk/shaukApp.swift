629b56e346dc59cf908493e60cdef3a8ab8ed4e5
Aryaman <aryaman99@gmail.com>
2026-04-26 20:02:34 +0400

    Initial Commit

---
+++ shauk/shaukApp.swift
@@ -0,0 +1,17 @@
+//
+//  shaukApp.swift
+//  shauk
+//
+//  Created by Aryaman on 4/26/26.
+//
+
+import SwiftUI
+
+@main
+struct shaukApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ shaukTests/shaukTests.swift
@@ -0,0 +1,19 @@
+//
+//  shaukTests.swift
+//  shaukTests
+//
+//  Created by Aryaman on 4/26/26.
+//
+
+import Testing
+@testable import shauk
+
+struct shaukTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
